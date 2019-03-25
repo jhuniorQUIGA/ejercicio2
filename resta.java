c76a0a077adcaa69a9ce209142a1b85d504215ee
ruth <naturexs58@gmail.com>
2019-03-25 19:49:04 -0400

    operacion resta

---
+++ resta.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Toshiba
+ */
+public class resta {
+    int resultado=0;
+    public int operacionResta(int numero1,int numero2){
+        if(numero1>=numero2){
+    int resultado=numero1-numero2;
+    return resultado;
+        }
+        else {
+        return resultado;
+        }
+         
+    }
+}
