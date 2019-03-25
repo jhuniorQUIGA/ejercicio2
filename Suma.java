1197b71eeb0198030db8725a269548f26c7fbebe
jhuniorQUIGA <48796947+jhuniorQUIGA@users.noreply.github.com>
2019-03-25 18:12:44 -0400

    Add files via upload

---
+++ Suma.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Suma here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Suma
+{
+    int resultado = 0;
+    public int operacionSuma(int numero1, int numero2)
+    {
+       resultado = numero1 + numero2;
+        return resultado;
+    }
+
+    
+}
