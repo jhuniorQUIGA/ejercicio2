c320a150495d13bf6f8647795e222ee1b3f23af4
ruth <naturexs58@gmail.com>
2019-03-25 19:52:53 -0400

    operacion resta

--- Suma.java
+++ Suma.java
@@ -20,6 +20,13 @@ public class Suma
        res = numero1/numero2;
        return res;
     }
-
+    public int operacionResta(int numero1,int numero2){
+        if(numero1>=numero2){
+    int resultado=numero1-numero2;
+    return resultado;
+        }
+        else {
+        return resultado;
+}
     
 }
