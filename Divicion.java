71fd617f02751c36de43575b610dede85d4ea089
Saisem <cjesusjhonatan@gmial.com>
2019-03-25 18:29:30 -0400

    OperacionDivicion

---
+++ Divicion.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Divicion here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Divicion
+{
+    
+    public int Divicion(int numero1 , int numero2)
+    {
+       int res = 0;
+       
+       res = numero1/numero2;
+       return res;
+    }
+
+   
+}
